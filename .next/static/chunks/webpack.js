4bb46027c002b9fd578b6bff821a1e2c75a58c71
Ace  Rosillo <acerosillo@users.noreply.github.com>
2022-11-10 14:21:57 +0000

    adding revalidate

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "afa66a4e53b52d54"; }
+/******/ 		__webpack_require__.h = function() { return "d45e88e9850a8688"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
